da1e49373bc6583c150db48fe9fdaf1e727407c4
Jon Dudley <98111478+duddb3@users.noreply.github.com>
2024-01-24 14:54:21 -0500

    Update pls_optimalcomp.m
    Switched criterion to predictive relevance (from mean squared error). Added permutations to improve robustness of component determination

--- pls_optimalcomp.m
+++ pls_optimalcomp.m
@@ -1,51 +1,58 @@
 function ncomp = pls_optimalcomp(X,Y)
+    % This function finds the optimal number of components to perform
+    % partial least squares regression for predictor variabls X and
+    % response variables Y.
 
-    K = 5; % Number of folds for cross-validation
+    % Number of folds for cross-validation
+    K = 5; 
+    % Number of permutations (generating different cross-validation sets)
+    P = 20; 
     % Choose upper bound on number of components to test. You could go up 
     % to rank(X), but that is very high
     ncomp = 30;
-    % Instantiate matrix that we will fill with mean square errors of each fold
-    % at each dimensionality
-    E = NaN(K,ncomp);
-    R2 = NaN(K,ncomp);
+    % Instantiate array that we will fill with the predictive relevance of 
+    % each fold at each dimensionality
+    Q2 = NaN(P,K,ncomp);
     
-    % Create cross-validation partitions for K-folds
-    n_subjects = size(Y,1);
-    C = cvpartition(n_subjects, 'KFold', K);    
+    
     
     for c=1:ncomp   % for each level of dimensionality
         txt = sprintf('Cross-validation of PLS-R model with %i components',c);
         fprintf(txt)
     
-        for k=1:K   % for each fold
-            txt2 = sprintf(': fold %i',k);
-            fprintf(txt2);
-            
-            % Get train and test set indices for fold k
-            traini = training(C,k);
-            testi = test(C,k);
-
-            % fit the PLSR model with ncomp PLS-scores on training fold
-            [~,~,~,~,b_pls,~] = plsregress(X(traini,:),Y(traini,:),c);
+        parfor p=1:P
+            % Create cross-validation partitions for K-folds
+            n_subjects = size(Y,1);
+            C = cvpartition(n_subjects, 'KFold', K);    
+            for k=1:K   % for each fold
+                
+                % Get train and test set indices for fold k
+                traini = training(C,k);
+                testi = test(C,k);
     
-            % Evaluate fit on test fold
-            yhat = [ones(sum(testi),1) X(testi,:)]*b_pls;
-    
-            % Compute mean square error
-            E(k,c) = mean((reshape(Y(testi,:),[],1)-yhat(:)).^2);
-    
-            fprintf(repmat('\b',1,length(txt2)))
+                % fit the PLSR model with ncomp PLS-scores on training fold
+                [~,~,~,~,b_pls,~] = plsregress(X(traini,:),Y(traini,:),c);
+                % Evaluate fit on test fold
+                yhat = [ones(sum(testi),1) X(testi,:)]*b_pls;
+                % Calculate the Predicted REsidual Sum of Squares
+                PRESS = sum((Y(testi,:)-yhat).^2,'all');
+                % Calculate the total sum of squares
+                TSS = sum((Y(testi,:)-mean(Y(testi,:))).^2,'all');
+                Q2(p,k,c) = 1 - PRESS/TSS;
+        
+            end
         end
     
         fprintf(repmat('\b',1,length(txt)));
     end
 
-    Ek = squeeze(mean(E));
-    [~,ncomp] = min(Ek);
+    Q2 = reshape(Q2,[],c);
+    Q2k = squeeze(mean(Q2));
+    [~,ncomp] = min(Q2k);
 
     figure,
-    errorbar(Ek,std(E)./sqrt(size(E,1)),'--ok')
-    ylabel('Observed Mean Square Error')
+    errorbar(Q2k,std(Q2)./sqrt(size(Q2,1)),'--ok')
+    ylabel('Observed Predictive Relevance (Q^2)')
     xlabel('Number of components')
 
 end
