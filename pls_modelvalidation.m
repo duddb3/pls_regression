a25b58f49021e0ac48988aa71206a211e26f9b44
Jon Dudley <98111478+duddb3@users.noreply.github.com>
2024-01-24 14:22:26 -0500

    Updates
    Added comments to pls_modelweights and pls_modelvalidation; changed pls_modelvalidation to use predictive relevance of model as criterion

--- pls_modelvalidation.m
+++ pls_modelvalidation.m
@@ -1,16 +1,37 @@
-function p_mse = pls_modelvalidation(X,Y,ncomp)
+function mdl = pls_modelvalidation(X,Y,ncomp)
+    % This function performs model validation via _permutation.
+    % Specifically, for each _permutation a new cross-validation partition
+    % set is defined and a random _permutation of the response variables is
+    % generated. Then for each fold, a partial least squares regression
+    % model is fit to the training set; the resulting model coefficients
+    % are used to generate the predicted response variables yhat (for the
+    % regular data) and yhat_perm (for the _permuted data). The mean squared
+    % error, Predicted REsidual Sum of Squares, and Total Sum of Squares,
+    % and predictive relevance of the model (Q^2) are then calculated for
+    % each _permutation. The resulting distributions are compared to yield
+    % the overall model significance.
 
-    nperms = 5000;
-    mse = NaN(nperms,1);
-    mseperm = NaN(nperms,1);
-    parfor p=1:nperms
-        yhat = NaN(size(Y));
-        yhatperm = yhat;
-        C = cvpartition(size(Y,1),'KFold',5);
+    % Set number of _permutations to perform
+    n_perms = 500;
+
+    % Set the number of folds for cross validation
+    K = 5;
+
+    % instantiate variables
+    TSS = NaN(n_perms,K);
+    TSS_perm = TSS;
+    PRESS = TSS;
+    PRESS_perm = TSS;
+    Q2 = TSS;
+    Q2_perm = TSS;
+
+    parfor p=1:n_perms
+        C = cvpartition(size(Y,1),'KFold',K);
     
         % shuffle Y
-        Yperm = Y(randperm(size(Y,1),size(Y,1)),:);
-        for k=1:C.NumTestSets           
+        Y_perm = Y(rand_perm(size(Y,1),size(Y,1)),:);
+
+        for k=1:C.NumTestSets   % do each fold        
             % Get the training and test sets
             trn = training(C,k);
             tst = test(C,k);
@@ -18,25 +39,47 @@ function p_mse = pls_modelvalidation(X,Y,ncomp)
             % Train the model with the kth partition data
             [~,~,~,~,BETA] = plsregress(X(trn,:),Y(trn,:),ncomp);
             % Fill in the predicted estimates for the kth test data
-            yhat(tst,:) = [ones(sum(tst),1) X(tst,:)]*BETA;
-    
+            yhat = [ones(sum(tst),1) X(tst,:)]*BETA;
+            % Calculate the Predicted REsidual Sum of Squares
+            PRESS(p,k) = sum((Y(tst,:)-yhat).^2,'all');
+            % Calculate the total sum of squares
+            TSS(p,k) = sum((Y(tst,:)-mean(Y(tst,:))).^2,'all');
+            Q2(p,k) = 1 - PRESS(p,k)/TSS(p,k);
+
             % Train the model with the kth partition data for shuffled Y
-            [~,~,~,~,BETA] = plsregress(X(trn,:),Yperm(trn,:),ncomp);
+            [~,~,~,~,BETA] = plsregress(X(trn,:),Y_perm(trn,:),ncomp);
             % Fill in the predicted estimates for the kth test data
-            yhatperm(tst,:) = [ones(sum(tst),1) X(tst,:)]*BETA;
+            yhat_perm = [ones(sum(tst),1) X(tst,:)]*BETA;
+            % Calculate the Predicted REsidual Sum of Squares
+            PRESS_perm(p,k) = sum((Y_perm(tst,:)-yhat_perm).^2,'all');
+            % Calculate the total sum of squares
+            TSS_perm(p,k) = sum((Y_perm(tst,:)-mean(Y_perm(tst,:))).^2,'all');
+            Q2_perm(p,k) = 1 - PRESS(p,k)/TSS(p,k);
+
+            
 
         end
-        mse(p) = mean((yhat-Y).^2,'all');
-        mseperm(p) = mean((yhatperm-Y).^2,'all');
     end
     
-    rng = linspace(min([mse;mseperm]),max([mse;mseperm]),100);
-    figure,histogram(mse,rng)
+
+    rng = linspace(min([Q2;Q2_perm]),max([Q2;Q2_perm]),100);
+    figure,histogram(Q2,rng);
     hold on
-    histogram(mseperm,rng)
-    p_mse = sum(mseperm<mean(mse))/nperms;
+    histogram(Q2_perm,rng)
+    
     hold off
     set(gcf,'Color','w')
     legend({'Model','Null Model'})
-    xlabel('Mean Squared Error')
+    xlabel('Predictive R^2')
+
+    mdl.TSS = TSS;
+    mdl.TSS_perm = TSS_perm;
+    mdl.PRESS = PRESS;
+    mdl.PRESS_perm = PRESS_perm;
+    mdl.Q2 = Q2;
+    mdl.Q2_perm = Q2_perm;
+    % The model p-value is the fraction of Q^2 values from the null
+    % distribution that are greater than the estimated real Q^2 value
+    mdl.pvalue = sum(Q2_perm>mean(Q2))/n_perms;
+
 end
--- pls_modelweights.m
+++ pls_modelweights.m
@@ -1,4 +1,24 @@
 function [b_mean_coeff,b_Z_coeff,b_P_coeff] = pls_modelweights(X,Y,ncomp)
+    % This function performs bootstrapping of partial least squares
+    % regression with inputs:
+    %
+    %   X: an n-by-v matrix of predictors where n is the number of 
+    %       observations (e.g., participants) and p is the number of
+    %       predictors.
+    %   Y: an n-by-m matrix of response variables where n is the number of
+    %       observations (e.g., participants) and m is the number of
+    %       response variables being predicted
+    %   ncomp: a scalar number that is the number of components to use in
+    %       the partial least squares regression
+    %
+    % The outputs of the function are:
+    %   b_mean_coeff: a v-by-m matrix of coefficient estimates for the
+    %       partial least squares regression
+    %   b_Z_coeff: a corresponding v-by-m matrix of z-statstics (i.e., how 
+    %       significantly each predictor variable contributes to the model
+    %       fit of each response variable)
+    %   b_P_coeff: the corresponding p-values (two-tailed)
+
 
     nboots = 5000;
     bs_b = bootstrp(nboots,@bootplsNCdim,X,Y,ncomp,'options',statset('UseParallel',true));
